6196248240751d9731f7e35133556a8ae7d2202e
JakeyWasTaken <75340712+JakeyWasTaken@users.noreply.github.com>
2022-07-14 01:07:48 +0100

    Testing Functions

--- src/Refer.lua
+++ src/Refer.lua
@@ -1,9 +1,18 @@
 local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
 local Utils = script:WaitForChild("Utils")
+local Private = script:WaitForChild("Private")
+local Services = script:WaitForChild("Services")
 
-local VersionSettings = require(Utils:WaitForChild("VersionSettings"))
+local VersionSettings = require(Private:WaitForChild("VersionSettings"))
+local UtilCache = {}
+local ServiceCache = {}
 
 local function VersionCheck()
+    if RunService:IsClient() then -- Prevent client running version check
+        return
+    end
+
     local DataTree = HttpService:GetAsync("https://raw.githubusercontent.com/JakeyWasTaken/Refer/main/datatree.json")
     DataTree = HttpService:JSONDecode(DataTree)
 
@@ -12,6 +21,34 @@ local function VersionCheck()
     end
 end
 
-VersionCheck()
+local function CacheUtils()
+    for _,Util in ipairs(Utils:GetChildren()) do
+        UtilCache[Util.Name] = require(Util)
+    end
+end
 
--- test
+local function CacheServices()
+    for _,Service in ipairs(Services:GetChildren()) do
+        ServiceCache[Service.Name] = require(Service)
+    end
+end
+
+VersionCheck()
+CacheUtils()
+CacheServices()
+
+local Refer = {}
+
+    Refer.Import = function(Name : string) : table
+        assert(UtilCache[Name],("[Refer] Cannot find util of name %s"):format(Name))
+
+        return UtilCache[Name]
+    end
+
+    Refer.GetService = function(Name : string) : table
+        assert(ServiceCache[Name],("[Refer] Cannot find service of name %s"):format(Name))
+
+        return ServiceCache[Name]
+    end
+
+return Refer
